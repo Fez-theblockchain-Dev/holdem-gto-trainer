8b52060814970449bb8ba47f9b4bf5618a53829d
Ramez <108371436+Fez-theblockchain-Dev@users.noreply.github.com>
2024-11-13 23:45:07 -0500

    new updates to files and new card shuffler function added in shuffler.ts file

--- holdem-gto-trainer/src/components/shuffler.ts
+++ holdem-gto-trainer/src/components/shuffler.ts
@@ -1,33 +1,32 @@
-import * as Cards from './Cards';
+// import * as Cards from './Cards';
 import { freshDeck } from './Cards';
-import { cardDeck } from './Cards'
-
+import { cardDeck } from './Cards';
 
 export default class Deck {
     private cards: cardDeck[];
+    
     constructor(cards = freshDeck()) {
-      this.cards = newIndex;
+        this.cards = cards;
     }
   
-    get numberOfCards() {
-      return this.cards.length
+    get numberOfCards(): number {
+        return this.cards.length;
     }
   
-    pop() {
-      return this.cards.shift()
+    pop(): cardDeck | undefined {
+        return this.cards.shift();
     }
   
-    push(card) {
-      this.cards.push(card)
+    push(card: cardDeck): void {
+        this.cards.push(card);
     }
 
-}
-
-public function shuffle() {
-    for (let i: number = this.numberOfCards - 1; i > 0; i--) {
-      const newIndex = Math.floor(Math.random() * (i + 1))
-      const oldValue != this.cards[newIndex]
-      // ... rest of shuffle implementation ...
+    shuffle(): void {
+        for (let i: number = this.numberOfCards - 1; i > 0; i--) {
+            const newIndex: number = Math.floor(Math.random() * (i + 1));
+            const oldValue: cardDeck = this.cards[newIndex];
+            this.cards[newIndex] = this.cards[i];
+            this.cards[i] = oldValue;
+        }
     }
-  }
 }
