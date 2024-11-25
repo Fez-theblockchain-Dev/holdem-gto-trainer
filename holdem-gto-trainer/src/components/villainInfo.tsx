a93ece901c0bd66b5ca7d3a5790d24a690e36794
Ramez Festek <108371436+Fez-theblockchain-Dev@users.noreply.github.com>
2024-11-25 17:41:45 -0500

    Update villainInfo.tsx

---
+++ holdem-gto-trainer/src/components/villainInfo.tsx
@@ -0,0 +1,13 @@
+
+type VillainInfo = {
+    name: string;
+}
+
+let villainInfo: VillainInfo[] = [
+    { name: "loose passive(calling station/fish)" },
+    { name: "tight aggressive(pro)" },
+    { name: "loose aggressive(maniac)" },
+    { name: "tight passive(nit)" }
+];
+
+console.log(villainInfo);
